07b8133bf253d438e3c46fab9c9d065174d02952
Andrei Rolando León Salas <andreileonsalas@gmail.com>
2021-03-11 13:19:42 -0600

    Andrei Leon Salas 201015265

--- 3.20.cpp
+++ 3.20.cpp
@@ -1,8 +1,18 @@
-// Your First C++ Program
 
-#include <iostream>
 
-int main() {
-    std::cout << "Hello World!";
-    return 0;
-}
+// Simple C++ program to display "Hello World" 
+  
+// Header file for input output functions 
+#include<iostream>  
+  
+using namespace std; 
+  
+// main function - 
+// where the execution of program begins 
+int main() 
+{ 
+    // prints hello world 
+    cout<<"Hello World"; 
+      
+    return 0; 
+} 
