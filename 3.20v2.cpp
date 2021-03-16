2017449dbb9ffbec59e045b5a36ca3b8621d3e82
Andrei Rolando León Salas <andreileonsalas@gmail.com>
2021-03-15 21:18:04 -0600

    Andrei Leon Salas 201015265

--- 3.20v2.cpp
+++ 3.20v2.cpp
@@ -6,7 +6,7 @@
 #define MIN_PID 300
 #define MAX_PID 5000
 
-int generarAleatorio();
+int get_random();
 int allocate_map(void);
 int allocate_pid();
 void release_pid();
@@ -14,16 +14,23 @@ void release_pid();
 bool* pid_map;
 
 int main() {
+    // initiate pid map
     if(allocate_map() == -1){
-        printf("no se creo el PID\n");
+        printf("unable to create the pid map\n");
     }
+
+    // sample pid for feature release
     int pid1, pid2;
+
+    // allocate pids
     for(int i = 0; i < 1000; i ++){
         int pid = allocate_pid();
         if(i == 3) pid1 = pid;
         if(i == 4) pid2 = pid;
         printf("PID: %d\n", pid);
     }
+
+    // release pids
     release_pid(pid1);
     release_pid(1000);
     release_pid(pid2);
@@ -32,14 +39,14 @@ int main() {
 
 int allocate_map(void){
     srand(time(0));
-    pid_map = malloc(sizeof(bool) * MAX_PID); 
+    pid_map = malloc(sizeof(bool) * MAX_PID); // yah, allocated extra 300 pid
     return pid_map == NULL ? -1 : 1;
 }
 
 int allocate_pid(){
-    int pid = generarAleatorio();
+    int pid = get_random();
     while(pid_map[pid] == true){
-        pid = generarAleatorio();
+        pid = get_random();
     }
     pid_map[pid] = true;
     return pid;
@@ -49,14 +56,14 @@ int allocate_pid(){
 void release_pid(int pid){
     if(pid_map[pid] == true){
         pid_map[pid] = false;
-        printf("Se solto el PID %d\n", pid);
+        printf("Release pid %d\n", pid);
     } else {
-        printf("PID %d No esta asociado\n", pid);
+        printf("PID %d is not associated with any process\n", pid);
     }
 }
 
 
-//Generar un numero al azar entre el minimo y maximo
-int generarAleatorio(){
+//to get a random number between max and min pid
+int get_random(){
     return (rand() % (MAX_PID - MIN_PID + 1) + MIN_PID);
 }
