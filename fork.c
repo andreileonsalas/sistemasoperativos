2f2b66388036bf2b14e49686c957ec3ae110f74e
Andrei Rolando León Salas <andreileonsalas@gmail.com>
2021-04-15 13:30:59 -0600

    aa

---
+++ fork.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include  <string.h>
+#include  <sys/types.h>
+
+int main()
+{
+    pid_t  pid;
+    int i = 0;
+        for (i=0; i < 3; i++) {
+            pid = fork();
+        if (pid = 0) fork();
+            printf("Hola \n");
+}
+
+}
