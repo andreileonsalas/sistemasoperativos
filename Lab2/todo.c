b339ac22ce7e8f9f26dc7cdf1bea614bfc24b90a
Andrei Rolando León Salas <andreileonsalas@gmail.com>
2021-05-21 19:20:16 -0600

    Laboratorio2

--- Lab2/todo.c
+++ Lab2/todo.c
@@ -18,9 +18,7 @@ int main()
     srand(time(NULL));
     int i, ch;
     
-    printf("\nIngrese las paginas, del 0 al 9: ");
     int n = 10;
-    scanf("%d", &n);
     //page sequences size enter like 1 2 3 4 5 1 1 1 1 1
     printf("\nEnter sequence numbers:");
     for (i = 0; i < n; i++)  //accepting sequence
